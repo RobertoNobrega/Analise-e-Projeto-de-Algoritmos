b618b8146dcd89e1ad0deb00c7ad82b623787f23
Roberto Nóbrega <r_t_4@hotmail.com>
2019-02-09 10:54:36 -0300

    Delete alght.h

--- alght.h
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-//extern void FMSort(int *vector, int begin, int medium, int end, int length);
-extern int FMSort(int ch, int *vector, int begin, int medium, int end, int length);
-extern void beginProgram();
